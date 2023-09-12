7e867a3b8cbcc849b592c6053f05e4c987393b13
Anas Jahangir <Anasjahangir.edu@gmail.com>
2023-09-13 04:44:30 +0500

    done

---
+++ Js/Assingment 8-17/app.js
@@ -0,0 +1,140 @@
+// 1
+
+// var userName = prompt("Enter your name")
+// alert("Hi, " + userName + "\n Prevent this page from creating additional dialogs.")
+
+// 2
+
+// var userTable = +prompt("Enter Table Number");
+
+// if (userTable && userTable !== null) {
+//     for (let i = 1; i <= 10; i++) {
+//         document.write(userTable +" x " + i + " = " + i*userTable + "<br>")
+//     }
+// }
+
+// 3
+
+// var userCity = prompt("Enter City Name");
+
+// if (userCity === "karachi") {
+//     alert("welcome to City of lights")
+// }
+
+// 4
+
+// var userGander = prompt("Enter Your Gander");
+
+// if (userGander === "male") {
+//     alert("Good Morning Sir.")
+// }else if (userGander === "female"){
+//     alert("Good Morning Ma’am.")
+// }else{
+//     alert("Apka Gander Nahi Mila")
+// }
+
+// 5
+
+// var userColor = prompt("Enter Color Name");
+
+// if ( userColor && userColor != null) {
+//         if (userColor === "red") {
+//             alert("vehicles must stop")
+//         }else if (userColor === "yellow") {
+//             alert("vehicles should get ready to move")
+//         }else if (userColor === "green") {
+//             alert("vehicles can move now")
+//         }
+     
+// }
+
+// 6
+
+// var userAge = prompt("Enter Your Age");
+
+// if (userAge <= 18) {
+//     alert("“You are welcome")
+// }
+
+// 7
+
+// var userFuel = +prompt("Enter Your Age");
+
+// if (userFuel <= 0.25) {
+//     alert("Please refill the fuel in your car")
+// }
+
+// 8  Run this script, & check whether alert message would be displayed or not. Record the outputs.
+
+// var a = 4;
+// if (++a === 5){
+//  alert("given condition for variable a is true");
+// }
+
+// // chalega
+
+//  var b = 82;
+// if (b++ === 83){
+//  alert("given condition for variable b is true");
+// }
+
+// // chalega 
+
+
+// var c = 12;
+// if (c++ === 13){
+//  alert("condition 1 is true");
+// }
+
+// // nahi chalega 
+
+
+// if (c === 13){
+//  alert("condition 2 is true");
+// }
+
+// // chalega 
+
+// if (++c < 14){
+//  alert("condition 3 is true");
+// }
+
+// // nahi chalega 
+
+// if(c === 14){
+//  alert("condition 4 is true");
+// }
+
+// // chalega 
+
+
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+
+
+// if (totalCost === laborCost + materialCost){
+//  alert("The cost equals");
+// }
+
+// // chalega 
+
+// if (true){
+//  alert("True");
+// }
+
+// // chalega 
+
+// if (false){
+//  alert("False");
+// }
+
+// // nahi chalega 
+
+// if("car" < "cat"){
+//  alert("car is smaller than cat");
+// }
+// // chalega 
+
+// 
+
