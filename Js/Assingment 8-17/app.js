97377cf659bf9828a62de30ab034a4e3892cb5f8
Anas Jahangir <Anasjahangir.edu@gmail.com>
2023-09-14 18:22:51 +0500

    added assingmets

--- Js/Assingment 8-17/app.js
+++ Js/Assingment 8-17/app.js
@@ -138,3 +138,62 @@
 
 // 
 
+// 9  Write a program to take input the marks obtained in three
+// subjects & total marks. Compute & show the resulting
+// percentage on your page. Take percentage & compute grade as
+// per following table:
+
+
+// var subject1 = prompt("Enter marks for Subject 1:");
+// var subject2 = prompt("Enter marks for Subject 2:");
+// var subject3 = prompt("Enter marks for Subject 3:");
+
+// subject1 = parseFloat(subject1);
+// subject2 = parseFloat(subject2);
+// subject3 = parseFloat(subject3);
+
+// var maxMarksPerSubject = 100; 
+
+// var totalMarks = subject1 + subject2 + subject3;
+
+// var percentage = (totalMarks / (maxMarksPerSubject * 3)) * 100;
+
+
+
+// if (percentage >= 80) {
+//     alert("Percentage: " + percentage.toFixed(2) + "%" + "\n Mubarak Ho A-One");
+// }else if (percentage >= 70 ){
+//     alert("Percentage: " + percentage.toFixed(2) + "%" + "\n A");
+// }else if (percentage >= 60 ){
+//     alert("Percentage: " + percentage.toFixed(2) + "%" + "\n B");
+// }else if (percentage < 60 ){
+//     alert("Percentage: " + percentage.toFixed(2) + "%" + "\n You Fail.");
+// }
+
+
+// 10
+
+// var item1Name = prompt("Enter the name of Item 1:");
+// var item2Name = prompt("Enter the name of Item 2:");
+// var item1Price = parseFloat(prompt("Enter the price of Item 1 (in PKR):"));
+// var item2Price = parseFloat(prompt("Enter the price of Item 2 (in PKR):"));
+// var item1Quantity = parseInt(prompt("Enter the ordered quantity of Item 1:"));
+// var item2Quantity = parseInt(prompt("Enter the ordered quantity of Item 2:"));
+// var shippingCharges = parseFloat(prompt("Enter the shipping charges (in PKR):"));
+
+// var totalCost = (item1Price * item1Quantity) + (item2Price * item2Quantity) + shippingCharges;
+
+// if (totalCost > 2000) {
+//     var discount = totalCost * 0.10; 
+//     totalCost -= discount;
+// }
+
+// document.write("<h2>Receipt</h2>");
+// document.write("<p><strong>Item 1:</strong> " + item1Name + " (Price: " + item1Price + " PKR, Quantity: " + item1Quantity + ")</p>");
+// document.write("<p><strong>Item 2:</strong> " + item2Name + " (Price: " + item2Price + " PKR, Quantity: " + item2Quantity + ")</p>");
+// document.write("<p><strong>Shipping Charges:</strong> " + shippingCharges + " PKR</p>");
+// document.write("<p><strong>Total Cost:</strong> " + totalCost.toFixed(2) + " PKR</p>");
+
+
+
+
